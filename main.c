593042766a478b981e463f8ada3229307d096dcb
TakingTheL <yliu6@stuy.edu>
2017-10-04 14:58:33 -0400

    added main.c

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+  char str[] = "shenanigans";
+  char chr = 'n';
+  printf("from myStrChr: \n %c\n", *myStrChr(str, chr));
+  printf("from strchr: \n %c\n", *strchr(str, chr));
+
+  return 0;
+}
--- stretcher.c
+++ stretcher.c
@@ -1,19 +1,6 @@
-#include <stdio.h>
-#include <string.h>
-
 char * myStrChr( char*s, char c){
   while(c - *s && *s){
     s ++;
   }
   return s;
 }
-
-int main(){
-  char str[] = "shenanigans";
-  char chr = 'n';
-  printf("from myStrChr: \n %c\n", *myStrChr(str, chr));
-  printf("from strchr: \n %c\n", *strchr(str, chr));
-
-  return 0;
-}
-    

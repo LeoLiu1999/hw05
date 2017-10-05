8f4791587e673a4826cebe31ad473d9db33b8af4
TakingTheL <yliu6@stuy.edu>
2017-10-05 14:42:23 -0400

    To do: makefile and linkage between main and my* files

--- main.c
+++ main.c
@@ -2,10 +2,31 @@
 #include <string.h>
 
 int main(){
-  char str[] = "shenanigans";
+  char mychr = 'n';
+  char mystr1[] = "shenanigans";
+  char mystr2[] = "stuff";
   char chr = 'n';
-  printf("from myStrChr: \n [%c]\n", myStrChr(str, chr));
-  printf("from strchr: \n [%c]\n", strchr(str, chr));
+  char str1[] = "shenanigans";
+  char str2[] = "stuff";
 
+  printf("Testing strlen:\n");
+  printf("from myStrLen: %d\n", myStrLen(mystr1));
+  printf("from : %d\n", strlen(str1));
+
+  printf("Testing strcpy:\n");
+  printf("from myStrCpy: [%s]\n", myStrCpy(mystr1, mystr2));
+  printf("from strcpy: [%s]\n",strcpy(str1, str2));
+  
+  printf("Testing strncat:\n");
+  printf("from myStrnCat: [%s]\n", myStrnCat(mystr1, mystr2, 3));
+  printf("from strncat: [%s]\n", strncat(str1, str2, 3));
+
+  printf("Testing strchr:\n");
+  printf("from myStrChr: [%c]\n", myStrChr(mystr1, mychr));
+  printf("from strchr: [%c]\n", strchr(str1, chr));
+  
+  printf("Testing strcmp:\n");
+  printf("from myStrCmp: %d\n", myStrCmp(mystr1, mystr2));
+  printf("from strcmp: %d\n", strcmp(str1, str2));
   return 0;
 }
--- myStrnCat.c
+++ myStrnCat.c
@@ -10,14 +10,3 @@ char * myStrnCat (char *dest, char *source, int n){
   dest[dest_length + n] = 0;
   return dest;
 }
-
-int main(){
-  char str1[] = "shenanigans";
-  char str2[] = "stuff";
-  printf("from myStrnCat: \n [%s]\n", myStrnCat(str1, str2, 3));
-  char str3[] = "shenanigans";
-  char str4[] = "stuff";
-  printf("from strncat: \n [%s]\n", strncat(str3, str4, 3));
-
-  return 0;
-}
